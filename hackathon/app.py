8cb2e84c9ef7a88854c19f35b74b97e17fddd497
Litery <s.litera@gmail.com>
2023-01-27 20:52:45 +0100

    Open CORS

--- hackathon/app.py
+++ hackathon/app.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from langchain import GoogleCalendarAPIWrapper
 
@@ -7,8 +8,17 @@ app = FastAPI()
 calendar = GoogleCalendarAPIWrapper()
 load_dotenv()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/prompt")
+@app.options("/prompt")
 async def prompt(query: str) -> dict:
     # return {"message": "Hello World"}
     return {"response": f"{calendar.run(query)}"}
